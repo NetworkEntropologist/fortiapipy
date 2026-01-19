973387ece2568ea567cd32e90c64db5472c53c6b
Jaco du Plessis <5507425+NetworkEntropologist@users.noreply.github.com>
2026-01-19 09:44:33 +0200

    Testing RTD config

--- fortiapipy/__init__.py
+++ fortiapipy/__init__.py
@@ -1,6 +1,10 @@
+"""Fortinet API client library for Python."""
+
 __author__ = "The Network Entropologist"
-__copyright__ = "(c) Oleg Butovich 2013-2024"
+__copyright__ = "(c)2026 The Network Entropologist"
 __version__ = "0.1.0"
 __license__ = "Apache-2.0"
+
+
 
 from .core import *
--- fortiapipy/core.py
+++ fortiapipy/core.py
@@ -1,4 +1,14 @@
+"""
+Fortinet API client library for Python.
 
+This is the core module that provides the main functionality for interacting 
+with Fortinet devices via their APIs.
+"""
+
+__author__ = "The Network Entropologist"
+__copyright__ = "(c) 2026 The Network Entropologist"
+__version__ = "0.1.0"
+__license__ = "Apache-2.0"
 
 
 import logging
